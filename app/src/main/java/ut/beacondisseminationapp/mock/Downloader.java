a1c76d7cc571357a8ddb2f05f45eb029a9e66801
RafiRashid <rafi.rashid@utexas.edu>
2014-11-27 17:17:07 -0600

    Update Downloader.java

--- app/src/main/java/ut/beacondisseminationapp/mock/Downloader.java
+++ app/src/main/java/ut/beacondisseminationapp/mock/Downloader.java
@@ -9,49 +9,78 @@ import java.util.TimerTask;
 *Rafi Rashid
 */
 public class Downloader {
-	long datarate; //in bits per second
-	int downloadStrategy; //0,1,2
-	ArrayList<Chunk> masterFile; 
-	ArrayList<Chunk> downloadedFile;
+	public Timer timer;
+	public long datarate; //in bits per second
+	public int downloadStrategy; //0,1,2
+	public ArrayList<Chunk> masterFile; 
+	public ArrayList<Chunk> downloadedFile;
+	public int current_chunk; 
+	public int new_chunk;
 	/*constructor*/
-	public Downloader(long rate, int strategy, ArrayList<Chunk> file){
-		datarate = rate;
-		downloadStrategy = strategy;
-		masterFile = file;		
+	public Downloader(long datarate, int downloadStrategy, ArrayList<Chunk> masterFile){
+		this.datarate = datarate;
+		this.downloadStrategy = downloadStrategy;
+		this.masterFile = masterFile;		
 	/*datarate setter*/	
-	public void set_datarate(long rate){
-		datarate = rate;
+	public void set_datarate(long datarate){
+		this.datarate = datarate;
 	}
 	/*strategy setter*/
-	public void set_strategy(int strategy){
-		downloadStrategy = strategy;
+	public void set_strategy(int downloadStrategy){
+		this.downloadStrategy = downloadStrategy;
 	}
 	/*datarate getter*/
 	public long get_datarate(void){
-		return datarate;
+		return this.datarate;
 	}
 	/*strategy getter*/
 	public int get_strategy(void){
-		return downloadStrategy;
+		return this.downloadStrategy;
+	}
+	//downloadtask for strategy 0
+	class DownloadTask0 extends TimerTask { 
+		public void run(){
+			while (new_chunk < current_chunk){ //move bits to move in one second
+				downloadedFile.get(new_chunk) = masterFile.get(new_chunk);
+				new_chunk++;
+			}
+			timer.cancel(); //terminate timer thread
+		}		
+
+	}
+	//downloadtask for strategy 1
+	class DownloadTask1 extends TimerTask {  
+		public void run(){
+			while (new_chunk > current_chunk){ 
+				new_chunk--;
+				downloadedFile.get(new_chunk) = masterFile.get(new_chunk);	
+			}
+			timer.cancel();
+		}		
+
+	}
+	//download task for strategy 2
+	class DownloadTask2 extends TimerTask { 
+		public void run(){
+			
+		}		
+
 	}
 	/*download method*/
 	public void	download(void){
-		int new_chunk = 0;
-		int current_chunk = 0;
+		timer = new Timer();
 		int bit_size = 0;
-		long length = masterFile.size();		
+		long length = masterFile.size(); //length of the masterfile	
 		if (downloadStrategy == 0){ //from front to back
-			while (new_chunk < length){
+			current_chunk = 0;
+			new_chunk = 0;
+			while (new_chunk < length){ 
 				while (bit_size < datarate){ //how many bits to move in a second
 					bit_size += masterFile.get(current_chunk).size*8;
-					current_chunk++;	
+					current_chunk++; //incrementing to the chunk needed
 				}
 				bit_size = 0;
-				Thread.sleep(1000); //waits 1 second
-				while (new_chunk < current_chunk && new_chunk < length){ //move bits to move in one second
-					downloadedFile.get(new_chunk) = masterFile.get(new_chunk);
-					new_chunk++;
-				}
+				timer.schedule(new DownloadTask0(),1000); 
 			}
 		}else if(downloadStrategy == 1){ //from back to front
 			current_chunk = length;
@@ -62,32 +91,32 @@ public class Downloader {
 					bit_size += masterFile.get(current_chunk).size*8;
 				}
 				bit_size = 0;
-				Thread.sleep(1000); //waits 1 second
-				while (new_chunk > current_chunk && new_chunk > 0){ //move bits for 1 sec
-					new_chunk--;
-					downloadedFile.get(new_chunk) = masterFile.get(new_chunk);
-					
-				}
+				timer.schedule(new DownloadTask1(),1000);
 			}
-		}else if(downloadStrategy == 2){ //randomly  move 
+		}else if(downloadStrategy == 2){ //randomly move 
 			Random rand = new Random();
 			int val = rand.nextInt(length);
 			Hashtable check = Hashtable(length);
 			Hashtable current = Hashtable(length);
-			while (new_chunk > 0){
+			while (!check.isEmpty()){ //while check hashtable is not empty
 				while (bit_size < datarate){ //how many bits to move in a second
-					bit_size += masterFile.get(current_chunk).size*8;
-					val = rand.nextInt(length);
-					check.remove(val);
+					bit_size += masterFile.get(val).size*8; //increase bit count
+					check.remove(val); //remove value from check hashtable
+					while(!check.containsValue(val)){ //while val is not in check, find another val
+						val = rand.nextInt(length);
+					}
 				}
-				bit_size = 0;
-				Thread.sleep(1000); //waits 1 second
-				while (check.get(val)!=current.get(val){ //move bits for 1 sec
+				//all values moved in ~second have been taken out of check
+				bit_size = 0;//reset bit counter size
+				while (!check.containsValue(val)){ //move bits for 1 sec
 					val = rand.nextInt(length);
-					downloadedFile.get(val) = masterFile.get(val);
+					if(!check.containsValue(val)&&current.containsValue(val)){
+						downloadedFile.get(val) = masterFile.get(val);
+					}
 				}
 			}
 		}
 	}
 }
+
 
